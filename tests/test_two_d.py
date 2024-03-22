7d6b218ab25dc6ce07ff37b13396e0d818e3a3b7
rebcabin <rebcabin@users.noreply.github.com>
2024-03-22 09:31:25 -0700

    convolution theorem working

--- tests/test_two_d.py
+++ tests/test_two_d.py
@@ -18,32 +18,34 @@ def test_test_itself():
     assert return_true()
 
 
+@pytest.mark.skip(reason="unneeded")
 def test_64_u8_img():
     fig, ax = plt.subplots(1, 5, figsize=(17, 17))
     images = ImageU8.images()
     for i, image in enumerate(images):
-        image.plot(ax, i)
+        image.plot_abs(ax, i)
     plt.show()
 
 
 def test_conjugate():
     a = SqMatC32(
-        np.array([[1+1j, 2], [4, 5]], dtype=np.complexfloating))
+        np.array([[1 + 1j, 2], [4, 5]], dtype=np.complexfloating))
     b = a.conjugate()
-    result = np.all(np.equal(b.data, [[1.-1.j, 2], [4, 5]]))
-    orig_not_changed = np.all(np.equal(a.data, [[1+1j, 2], [4, 5]]))
+    result = np.all(np.equal(b.data, [[1. - 1.j, 2], [4, 5]]))
+    orig_not_changed = np.all(np.equal(a.data, [[1 + 1j, 2], [4, 5]]))
     assert result and orig_not_changed
 
 
 def test_mul():
     a = SqMatC32(
-        np.array([[1+1j, 2], [4, 5]], dtype=np.complexfloating))
+        np.array([[1 + 1j, 2], [4, 5]], dtype=np.complexfloating))
     b = a.conjugate()
-    result = np.all(np.equal(a.mul(b).data, [[2, 4], [16, 25]]))
-    orig_not_changed = np.all(np.equal(a.data, [[1+1j, 2], [4, 5]]))
+    result = np.all(np.equal(a.elementwise_mul(b).data, [[2, 4], [16, 25]]))
+    orig_not_changed = np.all(np.equal(a.data, [[1 + 1j, 2], [4, 5]]))
     assert result and orig_not_changed
 
 
+@pytest.mark.skip(reason="known bugs")
 def test_dft():
     fig, ax = plt.subplots(4, 5, figsize=(17, 17))
     images = ImageU8.images()  # [3:]  # the others are too slow
@@ -51,11 +53,68 @@ def test_dft():
     dfts = [c.dft().chop() for c in cplxs]
     idfts = [d.dft(inverse=True).chop() for d in dfts]
     for i, img in enumerate(images):
-        img.plot(ax, (0, i))
-        cplxs[i].plot(ax, (1, i), 'Original as complex')
-        dfts[i].plot(ax, (2, i), 'DFT')
-        idfts[i].plot(ax, (3, i), 'InvDFT(DFT)')
+        img.plot_abs(ax, (0, i))
+        cplxs[i].plot_abs(ax, (1, i), '|complex img|')
+        dfts[i].plot_abs(ax, (2, i), '|DFT|')
+        idfts[i].plot_abs(ax, (3, i), '|InvDFT(DFT)|')
     plt.show()
+
+
+def test_fft2():
+    fig, ax = plt.subplots(5, 5, figsize=(17, 17))
+    images = ImageU8.images()  # [3:]  # the others are too slow
+    cplxs = [i.to_Complex() for i in images]
+    ffts = [c.fft() for c in cplxs]
+    iffts = [f.ifft().chop() for f in ffts]
+    for i, img in enumerate(images):
+        cplxs[i].plot_abs(ax, (0, i), '|complex img|')
+        ffts[i].plot_abs(ax, (1, i), '|DFT|')
+        ffts[i].plot_arg(ax, (2, i), '/_DFT')
+        iffts[i].plot_abs(ax, (3, i), '|IDFT(DFT)|')
+        iffts[i].plot_arg(ax, (4, i), '/_IDFT(DFT)')
+    plt.show()
+
+
+# https://setosa.io/ev/image-kernels/
+
+kernel_emboss = \
+    SqMatC32(np.array(
+        [
+            [-2, -1, 0],
+            [-1, 1, 1],
+            [0, 1, 2]],
+        dtype=np.complexfloating))
+
+kernel_blur = \
+    SqMatC32(np.array([
+        [1 / 16., 1 / 8., 1 / 16.],
+        [1 / 8., 1 / 4., 1 / 8.],
+        [1 / 16., 1 / 8., 1 / 16.]],
+        dtype=np.complexfloating))
+
+kernel_wipe = \
+    SqMatC32(np.array([
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]],
+        dtype=np.complexfloating))
+
+
+def test_convolution():
+    images = ImageU8.images()
+    cplxs = list(reversed([i.to_Complex() for i in images]))
+    convos = [c.convolve_brute_force(kernel_blur)
+              for c in cplxs]
+    thrms = [c.convolution_theorem(kernel_blur) for c in cplxs]
+    fig, ax = plt.subplots(5, 5, figsize=(17, 17))
+    for i, img in enumerate(images):
+        cplxs[i].plot_abs(ax, (0, i), '|complex img|')
+        convos[i].plot_abs(ax, (1, i), '|convo blur|')
+        convos[i].plot_arg(ax, (2, i), '/_convo blur')
+        thrms[i].plot_abs(ax, (3, i), '|theorem blur|')
+        thrms[i].chop().plot_arg(ax, (4, i), '/_theorem blur')
+    plt.show()
+
 
 def test_cast():
     temp = ImageU8.bw_square_inset_white(N=64)
--- two_d.py
+++ two_d.py
@@ -1,7 +1,10 @@
 """motivated by https://github.com/rebcabin/Digital-Image-Processing"""
+from unittest import result
 
 import numpy as np
 from dataclasses import dataclass
+
+
 # import matplotlib.pyplot as plt
 # import PIL.Image as Image
 # import math
@@ -51,34 +54,115 @@ class SqMat:
         assert self.is_square()
         return self.H()
 
-    def is_suitable_for_fft(self) -> bool:
+    def is_square_power_of_two(self) -> bool:
         return self.is_square() and self.is_p2()
 
-    def plot(self, ax, idx, title:str) -> None:
+    def plot_abs(self, ax, idx, title: str) -> None:
         raise NotImplementedError
 
 
 @dataclass
 class SqMatC32(SqMat):
 
+    @staticmethod
+    def zeros(N: int) -> "SqMatC32":
+        result = SqMatC32(np.zeros((N, N),
+                                   dtype=np.complexfloating))
+        return result
+
     def conjugate(self):
         temp = np.conj(self.data)
         result = SqMatC32(temp)
         return result
 
-    def mul(self, other: "SqMatC32") -> "SqMatC32":
+    def elementwise_mul(self, other: "SqMatC32") -> "SqMatC32":
+        """Element-wise multiplication (Hadamard product)."""
         result = SqMatC32(self.data * other.data)
         return result
 
-    def dft(self, inverse: bool=False) -> "SqMatC32":
-        """https://en.wikipedia.org/wiki/Discrete_Fourier_transform
+    def fft(self) -> "SqMatC32":
+        """Why the shifts?"""
+        fft = np.fft.ifftshift(self.data)
+        fft = np.fft.fft2(fft)
+        fft = np.fft.fftshift(fft)
+        result = SqMatC32(fft)
+        return result
+
+    def ifft(self) -> "SqMatC32":
+        """Why the shifts?"""
+        ifft = np.fft.ifftshift(self.data)
+        ifft = np.fft.ifft2(ifft)
+        ifft = np.fft.fftshift(ifft)
+        result = SqMatC32(ifft)
+        return result
+
+    def convolve_brute_force(self, kernel: "SqMatC32") -> "SqMatC32":
+        # TODO: tested only for 3x3 for now.
+        # ML-style -- reduces the size of self by K // 2 on
+        # top, bottom, left, and right.
+        N = self.N()
+        K = kernel.N()
+        assert K == 3
+        R = K // 2
+        output = SqMatC32.zeros(N)
+        for h in range(R, self.H() - R):
+            for w in range(R, self.W() - R):
+                acc = 0. + 0.j
+                for hk in range(kernel.H()):
+                    for wk in range(kernel.W()):
+                        acc += kernel.data[hk, wk] \
+                            * self.data[h + hk - R, w + wk - R]
+                output.data[h, w] = acc
+        return output
+
+    def convolution_theorem(self, kernel: "SqMatC32") -> "SqMatC32":
+
+        def pad_kernel(kernel: "SqMatC32") -> "SqMatC32":
+            """TODO Tested only for 3x3 kernel."""
+            N = self.N()
+            K = kernel.N()
+            # If N == 32, the half-way index is N // 2 == 16.
+            # That index begins the second half of the array.
+            # An odd-order kernel should stride that index.
+            #
+            # Looks like the starting index for the centered
+            # boxcar is (N // 2) - (K // 2).
+            #
+            #   0         1         2         3
+            #   012345678901234567890123456789012
+            #                   V
+            #  .---=---=---=---=---=---=---=---=.
+            #  |              .-=-.             |
+            #  | <----15----> |K=3| <--- 14 --> |
+            #  |              `-=-'             |
+            # ...                              ...
+            #  |                                |
+            #  |                                |
+            #  `--------------------------------'
+            temp = SqMatC32.zeros(N)
+            L = (N // 2) - (K // 2)
+            temp.data[L: L + K, L: L + K] = kernel.data
+            kfft = temp.fft()
+            sorc = self.fft()
+            temp2 = kfft.elementwise_mul(sorc)
+            result = temp2.ifft()
+            return result
+
+        return pad_kernel(kernel)
+
+    def dft(self, inverse: bool = False) -> "SqMatC32":
+        """TODO: broken
+        https://en.wikipedia.org/wiki/Discrete_Fourier_transform
         O(n^2), slow."""
         assert self.is_square() and self.is_p2()
         N: int = self.data.shape[0]
+        it = np.fft.ifftshift(self.data)
         if not inverse:
-            result: SqMatC32 = SqMatC32(np.fft.fft2(self.data))
+            it = np.fft.fft2(it)
         else:
-            result: SqMatC32 = SqMatC32(np.fft.ifft2(self.data))
+            it = np.fft.ifft2(it)
+        it = np.fft.fftshift(it)
+        result = SqMatC32(it)
         # result = SqMatC32(np.zeros((N, N), dtype=np.complexfloating))
         # for ky in range(N):
         #     for kx in range(N):
@@ -98,15 +182,23 @@ class SqMatC32(SqMat):
         assert self.is_square() and self.is_p2()
         moduli = np.abs(self.data)
         N: int = self.data.shape[0]
-        temp = np.zeros((N, N), dtype=np.complexfloating)
+        temp = SqMatC32.zeros(N)
         for ky in range(N):
             for kx in range(N):
-                temp[kx, ky] = (0. + 0.j) if moduli[kx, ky] < epsilon else moduli[kx, kx]
-        result = SqMatC32(temp)
-        return result
+                temp.data[kx, ky] = \
+                    (0. + 0.j) if moduli[kx, ky] < epsilon else moduli[kx, kx]
+        return temp
 
-    def plot(self, ax, idx, title='complex modulus') -> None:
-        it = np.absolute(self.data)
+    def plot_abs(self, ax, idx, title='modulus') -> None:
+        it = np.abs(self.data)
+        it = np.log(1 + it)
+        vmin = np.min(it)
+        vmax = np.max(it)
+        ax[idx].imshow(it, cmap='gray', vmin=vmin, vmax=vmax)
+        ax[idx].set_title(title)
+
+    def plot_arg(self, ax, idx, title='argument') -> None:
+        it = np.angle(self.data)
         vmin = np.min(it)
         vmax = np.max(it)
         ax[idx].imshow(it, cmap='gray', vmin=vmin, vmax=vmax)
@@ -117,7 +209,7 @@ class SqMatC32(SqMat):
 class ImageU8(SqMat):
 
     def to_Complex(self):
-        assert(np.can_cast(self.data, np.complexfloating))
+        assert (np.can_cast(self.data, np.complexfloating))
         result = SqMatC32(self.data.astype(np.complexfloating))
         return result
 
@@ -125,19 +217,19 @@ class ImageU8(SqMat):
     def black_square(N: int) -> "ImageU8":
         """factory"""
         it = ImageU8(ImageU8._black_square_nda(N))
-        assert it.is_suitable_for_fft()
+        assert it.is_square_power_of_two()
         return it
 
     @staticmethod
     def _white_square_nda(N: int) -> np.ndarray:
         result = np.iinfo(np.uint8).max \
-            * np.ones((N, N), dtype=np.uint8)
+                 * np.ones((N, N), dtype=np.uint8)
         return result
 
     @staticmethod
     def _black_square_nda(N: int) -> np.ndarray:
         result = np.iinfo(np.uint8).min \
-            * np.zeros((N, N), dtype=np.uint8)
+                 * np.zeros((N, N), dtype=np.uint8)
         return result
 
     @staticmethod
@@ -146,7 +238,7 @@ class ImageU8(SqMat):
         assert (N % 4 == 0)
         it = ImageU8.black_square(N)
         # Start a quarter of the way from top-left (tl).
-        tl: int = N // 4         # e.g. 8 for N=32
+        tl: int = N // 4  # e.g. 8 for N=32
         # End three quarters of the way from bottom-right (br).
         br: int = tl + (N // 2)  # 8 + 16 = 24 for N=32
         N2 = N // 2
@@ -155,22 +247,22 @@ class ImageU8(SqMat):
 
     def bw_square_resized_white_portions(self) -> list["ImageU8"]:
         """factory"""
-        assert self.is_suitable_for_fft()
+        assert self.is_square_power_of_two()
         result = []
-        N = self.N()             # e.g., 32
-        side = N // 2            # 16, 8, 4
+        N = self.N()  # e.g., 32
+        side = N // 2  # 16, 8, 4
         while side >= 4:
-            side = side // 2     #  8,  4,  2
-            j = (N - side) // 2  #  4,  6,  7  (16-8)//2=4, (16-4)//2=6, (16-2)//2=7
-            k = j + side         # 12, 10,  9  4+8=12, 6+4=10, 7+2=9
-                                 # The sum is always 16.
+            side = side // 2  # 8,  4,  2
+            j = (N - side) // 2  # 4,  6,  7  (16-8)//2=4, (16-4)//2=6, (16-2)//2=7
+            k = j + side  # 12, 10,  9  4+8=12, 6+4=10, 7+2=9
+            # The sum is always 16.
             temp = ImageU8.black_square(N)
             temp.data[j:k, j:k] = ImageU8._white_square_nda(side)
             result.append(temp)
         return result
 
     def avg_pool(self, new_size: int) -> "ImageU8":
-        assert self.is_suitable_for_fft()
+        assert self.is_square_power_of_two()
         N = self.N()
         assert divides(new_size, N)
         step = N // new_size
@@ -179,8 +271,8 @@ class ImageU8(SqMat):
             for w in range(result.W()):
                 result.data[h, w] = \
                     np.mean(self.data[
-                            h * step : (h + 1) * step,
-                            w * step : (w + 1) * step])
+                            h * step: (h + 1) * step,
+                            w * step: (w + 1) * step])
         return result
 
     @staticmethod
@@ -193,9 +285,8 @@ class ImageU8(SqMat):
             result.append(temp.avg_pool(size))
         return result
 
-    def plot(self, ax, idx, title='Original image') -> None:
+    def plot_abs(self, ax, idx, title='Original image') -> None:
         vmin = self.data.min()
         vmax = self.data.max()
         ax[idx].imshow(self.data, cmap='gray', vmin=vmin, vmax=vmax)
         ax[idx].set_title(title)
-
